d509cd1c72a748d30c5a28540da9c237b4dc413c
qkrxogmla <suk05553@naver.com>
2025-07-04 17:41:04 +0900

    5번

--- Taehee/chap5/05-a.js
+++ Taehee/chap5/05-a.js
@@ -1,6 +1,13 @@
-function solution(a) {
-  console.log(a.sort((a, b) => a - b));
-  return a;
+function solution(arr1, arr2) {
+  var answer = [...new Array(arr1.length)].map((_, i) =>
+    new Array(arr2[0].length).fill(0)
+  );
+  for (var n = 0; n < arr1.length; n++) {
+    for (var i = 0; i < arr2[0].length; i++) {
+      for (var a = 0; a < arr2.length; a++) {
+        answer[n][i] += arr1[n][a] * arr2[a][i];
+      }
+    }
+  }
+  return answer;
 }
-a = [1, 3, 200, 4, 5];
-solution(a);
