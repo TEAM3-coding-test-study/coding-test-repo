e3729042f47fb3a24e5aabf6360013f822409cd1
glyph8 <newdy0808@gmail.com>
2025-07-10 00:53:36 +0900

    주식 가격 GG

--- dongyun/chap6/05-a.js
+++ dongyun/chap6/05-a.js
@@ -1,27 +1,47 @@
 function solution(prices) {
-  var answer = [];
+  var answer = Array(prices.length).fill(0);
 
   const stack = [];
   let top = -1;
 
   for (const [index, price] of prices.entries()) {
+    console.log("바깥 반복 시작 index = ", index)
     if (top === -1) {
       stack.push(price);
       top++;
-      continue;
     }
-    if (stack[top] <= price) {
+    else if (stack[top] <= price) {
+      //이전 값보다 주식 가격이 증가 or 유지
       stack.push(price);
       top++;
-      continue;
-    }
-    while (stack[top] <= price || top === -1)
-      if (stack[top] > price) {
-        stack.pop();
-        top--;
-        continue;
-      }
-  }
 
+      // answer[index]++;
+    }
+    else if (stack[top] > price) {
+      //이전 값보다 주식 가격이 하락
+      stack.pop();
+      top--;
+
+      // answer[index] = 1;
+    }
+    for (let i = 0; i < prices.length; i++) {
+      if (i < index) {
+        //과거 주식 가격
+        if (prices[i] < stack[top]) {
+          answer[i]++;
+        }
+      }
+      if (i === index) {
+        //현재 주식 가격
+        answer[i] = 1;
+      }
+    }
+    answer[index]++;
+  }
+  answer[prices.length - 1] = 0;
+  console.log(answer)
   return answer;
 }
+
+solution([1, 2, 3, 2, 3]);
+// solution([5, 4, 3, 2, 1])
