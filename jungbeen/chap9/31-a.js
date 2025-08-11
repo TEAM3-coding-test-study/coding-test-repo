3fdff8dd5baa856078d0545f4c263c8371325e3a
TakioN <jungbeen0724@gmail.com>
2025-08-11 15:09:39 +0900

    review chap9

--- jungbeen/chap9/31-a.js
+++ jungbeen/chap9/31-a.js
@@ -1,42 +1,53 @@
 class Queue {
   items = [];
   rear = -1;
-  front = 0;
+  front = -1;
 
   push(val) {
+    this.rear++;
     this.items.push(val);
   }
   pop() {
-    this.rear++;
+    this.front++;
     return this.items[this.front];
   }
   isEmpty() {
-    return !(front - rear);
+    return this.rear - this.front === 0;
   }
 }
 
-function solution(info, edges) {
-  var answer = 0;
-  let ha = new Map();
-  const q = new Queue();
-
+function makeTree(info, edges) {
+  const tree = Array.from({ length: info.length }, () => []);
   for (let e of edges) {
-    if (ha.has(e[0])) {
-      ha.set(e[0], [...ha.get(e[0]), e[1]]);
-    } else {
-      ha.set(e[0], [e[1]]);
-    }
+    tree[e[0]].push(e[1]);
   }
+  return tree;
+}
 
-  q.push([0, 1, 0]);
-
+function solution(info, edges) {
+  const t = makeTree(info, edges);
+  const q = new Queue();
+  let answer = 0;
+  q.push([0, 0, 1, new Set()]);
   while (!q.isEmpty()) {
-    let [node, sh, wo] = q.pop();
-    let avail = ha.get(node);
-    for (let i of avail) {
-      q.push(i);
+    const [nodeNum, wolfCnt, lambCnt, visited] = q.pop();
+
+    for (let i of t[nodeNum]) visited.add(i);
+
+    for (let i of visited) {
+      if (info[i]) {
+        if (wolfCnt !== lambCnt - 1) {
+          const newVisited = new Set(visited);
+          newVisited.delete(i);
+          q.push([i, wolfCnt + 1, lambCnt, newVisited]);
+        }
+      } else {
+        const newVisited = new Set(visited);
+        newVisited.delete(i);
+        q.push([i, wolfCnt, lambCnt + 1, newVisited]);
+      }
     }
-    q.push();
+    answer = lambCnt;
   }
   return answer;
 }
@@ -56,6 +67,23 @@ console.log(
       [6, 5],
       [4, 6],
       [8, 9],
+    ]
+  )
+);
+console.log(
+  solution(
+    [0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0],
+    [
+      [0, 1],
+      [0, 2],
+      [1, 3],
+      [1, 4],
+      [2, 5],
+      [2, 6],
+      [3, 7],
+      [4, 8],
+      [6, 9],
+      [9, 10],
     ]
   )
 );
