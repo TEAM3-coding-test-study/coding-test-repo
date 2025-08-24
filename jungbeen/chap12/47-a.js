3a53c88168fab324e999dca40c2d4ad58e1d124a
TakioN <jungbeen0724@gmail.com>
2025-08-25 08:41:44 +0900

    review 47

--- jungbeen/chap12/47-a.js
+++ jungbeen/chap12/47-a.js
@@ -1,3 +1,20 @@
-function solution(N) {}
+function bt(start, included, sum, resultArr, maximum) {
+  if (sum === 10) {
+    resultArr.push(included);
+    return;
+  }
 
-console.log(solution());
+  for (let i = start; i <= maximum; i++) {
+    if (sum + i <= 10) {
+      bt(i + 1, included.concat(i), sum + i, resultArr, maximum);
+    }
+  }
+}
+
+function solution(N) {
+  const rs = [];
+  bt(1, [], 0, rs, N);
+  return rs;
+}
+
+console.log(solution(7));
