578016de68f167caafca903e8899c7c5d3b9f9eb
glyph8 <newdy0808@gmail.com>
2025-09-02 12:14:49 +0900

    64 완료

--- dongyun/chap14/64-a.js
+++ dongyun/chap14/64-a.js
@@ -1,23 +1,51 @@
 function solution(n) {
     const arr = [...new Array(n)].map((_)=> new Array(n).fill(0));
 
-    const dy = [-1, -1, -1, 0, 0, 1, 1, 1];
-    const dx = [-1, 0, 1, -1, 1, -1, 0, 1];
+    const dy = [0, 1, 0, -1];
+    const dx = [1, 0, -1, 0];
 
-    const determinTurn = (y, x) =>{
-        
-        if(y < n && x < n){
-
+    const checkIndexOut = (y, x)=>{
+        if(y < 0 || y >= n || x < 0 || x >= n){
+            return false;
         }
-        
-        // 우상단 코너
-        if(y >= n && x < n){
-            return 
-        }
+        return true;
     }
 
-    const move = (y, x) =>{
+    const determinTurn = (y, x, dir) =>{
+        if(checkIndexOut(y + dy[dir], x + dx[dir]) && arr[y + dy[dir]][x + dx[dir]] === 0){
+                return [y + dy[dir], x + dx[dir], dir]
+            }
+
+        for(let i = 0; i < 4; i++){
+            // const elem = ;
+            if(checkIndexOut(y + dy[i], x + dx[i]) && arr[y + dy[i]][x + dx[i]] === 0){
+                return [y + dy[i], x + dx[i], i]
+            }
+        } 
         
+        // 모든 방향을 갈 수 없을 때 종료
+        return [-1, -1, -1];
+    }
+
+ 
+    
+    let nowNumber = 1;
+    let x = 0;
+    let y = 0;
+    let dir = 0;
+
+    arr[y][x] = nowNumber;
+
+    while(true){
+        const [ny, nx, ndir] = determinTurn(y,x, dir);
+        if(ny === -1 && nx === -1){
+            return arr;
+        }
+        nowNumber++;
+        y = ny;
+        x = nx;
+        dir = ndir;
+        arr[y][x] = nowNumber; 
     }
 
 }
