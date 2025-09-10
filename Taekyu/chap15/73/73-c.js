e9fcd83de4e645beae60f00c4c3ae140c55b6386
Chris Ahn <chrisahn1007@gmail.com>
2025-09-10 14:52:05 +0900

    comp : 73

---
+++ Taekyu/chap15/73/73-c.js
@@ -0,0 +1,8 @@
+// 교재 풀이
+function solution(n) {
+  const fib = [0, 1]; // F(0) = 0, F(1) = 1
+  for (let i = 2; i <= n; i++) {
+    fib.push((fib[i - 1] + fib[i - 2]) % 1234567);
+  }
+  return fib[n];
+}
