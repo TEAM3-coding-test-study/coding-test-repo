5fbe50548bcd26f9792a5530d52a810a1db80d9e
qkrxogmla <suk05553@naver.com>
2025-09-19 16:22:03 +0900

    85

---
+++ Taehee/chap16/85-a.js
@@ -0,0 +1,20 @@
+function solution(n, stations, w) {
+  var answer = 0;
+  let start = 1;
+  let end = 0;
+  for (let i = 0; i <= stations.length; i++) {
+    if (i != 0) {
+      start = stations[i - 1] + w + 1;
+    }
+    if (i == stations.length) {
+      end = n;
+    } else {
+      end = stations[i] - w - 1;
+    }
+    if (start > end) {
+      continue;
+    }
+    answer += Math.ceil((end - start + 1) / (2 * w + 1));
+  }
+  return answer;
+}
